3de105da88117a8114d92e17c9896c0ce3834623
unknown <yura.rossol@gmail.com>
2022-05-20 02:15:51 +0300

    trying to put it to work

--- main.js
+++ main.js
@@ -4,12 +4,16 @@ const btn = document.getElementById('submit')
 
 
 async function getAnswer() {
-    let link = linkInput.value
+    let link = "https://zno-comment-proxy.herokuapp.com/" + linkInput.value
     let task = taskInput.value
 
     let htmlContent = await fetch(link)
     let r = await htmlContent.text()
-    console.log(r)
+
+    let el = document.createElement('html')
+    el.innerHTML = r
+
+    console.log(el.querySelector("img"))
 }
 
 btn.addEventListener('click', getAnswer)
