54db8a59fc1bc83217412979a3629c1171f30193
unknown <yura.rossol@gmail.com>
2022-05-20 16:41:31 +0300

    should work

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const linkInput = document.getElementById('link')
 const taskInput = document.getElementById('task')
 const btn = document.getElementById('submit')
+const iframe = document.getElementById("iframe")
 
 
 async function getAnswer() {
@@ -16,7 +17,21 @@ async function getAnswer() {
     let id = "q" + task
 
     let explanation = el.querySelector("div #" + id).querySelector("div .explanation")
-    console.log(explanation)
+    let images = explanation.querySelectorAll("img")
+    for (const image of images) {
+        let oldSrc = image.getAttribute("src")
+        let newSrc = "https://zno.osvita.ua" + oldSrc
+        image.setAttribute("src", newSrc)
+    }
+    explanation.setAttribute("style", '')
+
+    let script = document.createElement("script")
+    script.setAttribute("src", 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-svg.js')
+    script.defer = true
+    explanation.appendChild(script)
+
+    iframe.setAttribute("srcdoc", explanation)
+
 }
 
 btn.addEventListener('click', getAnswer)
